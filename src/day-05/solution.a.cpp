2c41a4d5c3e97d11cc2c568d3cd8fa4c76823867
Florian Walter <nairolf.retlaw@gmail.com>
2022-12-05 06:22:30 +0100

    add solution for day 5

---
+++ src/day-05/solution.a.cpp
@@ -0,0 +1,61 @@
+#include "base.hpp"
+
+#include <cstdio>
+#include <stack>
+#include <string>
+
+int main() {
+  vector<vector<char>> stacks;
+
+  while (1) {
+    string input;
+    getline(cin, input);
+
+    if (!cin || input.empty())
+      break;
+
+    int count = (input.length() + 1) / 4;
+    stacks.resize(count);
+
+    for (int i = 0; i < count; i++) {
+      int idx = i * 4 + 1;
+      char c = input[idx];
+
+      if (c == ' ' || (c >= '0' && c <= '9'))
+        continue;
+
+      stacks[i].insert(stacks[i].end(), c);
+    }
+  }
+
+  cout << stacks << endl;
+
+  while (1) {
+    string rearrangement;
+    getline(cin, rearrangement);
+
+    if (!cin)
+      break;
+
+    int count, from_idx, to_idx;
+    sscanf(rearrangement.c_str(), "move %d from %d to %d", &count, &from_idx,
+           &to_idx);
+
+    auto &from = stacks[from_idx - 1];
+    auto &to = stacks[to_idx - 1];
+
+    for (int i = 0; i < count; i++) {
+      char c = from[count - i - 1];
+      cout << "move " << c << " from " << from_idx << " to " << to_idx << endl;
+
+      from.erase(from.begin());
+      to.insert(to.begin(), c);
+    }
+  }
+
+  for (auto &stack : stacks) {
+    cout << stack[0];
+  }
+  cout << endl;
+  return 0;
+}
