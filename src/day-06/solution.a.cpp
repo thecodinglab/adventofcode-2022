6964f0c37b4443efff92dc56c4889bbdca3c5a33
Florian Walter <nairolf.retlaw@gmail.com>
2022-12-06 06:14:42 +0100

    add solution for day 6

---
+++ src/day-06/solution.a.cpp
@@ -0,0 +1,25 @@
+#include "base.hpp"
+
+int main() {
+  string val;
+  cin >> val;
+
+  int start = 0;
+  int window_size = 4;
+
+  for (int j = 0; j < val.length() - window_size; j++) {
+    set<char> characters;
+    for (int i = start; i < (start + window_size); i++) {
+      characters.insert(val[i]);
+    }
+
+    if (characters.size() == window_size) {
+      cout << start + window_size << endl;
+      break;
+    } else {
+      start++;
+    }
+  }
+
+  return 0;
+}
